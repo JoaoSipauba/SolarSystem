4a5b04cae318bf519011a769af28e9399851f4d6
joao.sipauba <joao.sipauba@ceuma.br>
2021-05-11 02:49:06 -0300

    feat: eslizando landing page

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import Landing from './src/pages/landing';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <Navigation />
+    </>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Landing" component={Landing} options={{ tabBarVisible: false }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
